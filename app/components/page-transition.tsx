d117192e03fd675ed4f9171d3edee4c1cbbbcdd9
Johncarlo Ababa <jcababa.drccebu@gmail.com>
2025-03-03 10:29:19 +0800

    Update page-transition.tsx

--- app/components/page-transition.tsx
+++ app/components/page-transition.tsx
@@ -15,7 +15,7 @@ export function PageTransition({ children }: { children: React.ReactNode }) {
         animate={{ opacity: 1 }}
         exit={{ opacity: 1 }}
         transition={{
-          duration: 0.15, // Reduced duration for snappier transitions
+          duration: 0.35, // Reduced duration for snappier transitions
           ease: "linear", // Changed to linear for smoother fade
         }}
       >
