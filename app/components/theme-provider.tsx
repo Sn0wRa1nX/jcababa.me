c3823e36c7182bcb2ae62d4ad8b77920e6eaafe7
Johncarlo Ababa <jcababa.drccebu@gmail.com>
2025-03-03 10:59:38 +0800

    Update theme-provider.tsx

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -4,7 +4,7 @@ import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false} {...props}>
+    <NextThemesProvider attribute="class" defaultTheme="white" enableSystem={false} {...props}>
       {children}
     </NextThemesProvider>
   )
