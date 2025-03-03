e097e1745406c44986b56cf53bb4fb0b45298166
Johncarlo Ababa <jcababa.drccebu@gmail.com>
2025-03-03 10:35:31 +0800

    Update page-transition.tsx

--- app/components/page-transition.tsx
+++ app/components/page-transition.tsx
@@ -12,7 +12,7 @@ export function PageTransition({ children }: { children: React.ReactNode }) {
       <motion.div
         key={pathname}
         initial={{ opacity: 0 }}
-        animate={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{
           duration: 0.9, // Reduced duration for snappier transitions
